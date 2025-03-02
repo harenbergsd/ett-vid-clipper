3914b6710dddd47ae47168dfd9cc28dee6d93822
Steve Harenberg <harenbergsd@gmail.com>
2025-03-02 18:02:45 -0500

    first push

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # ett-vid-clipper
-Code for automatically clipping points from Eleven Table Tennis recordings
+
+This code is used for automatically finding and extracting clips of [Eleven Table Tennis (ETT)](https://elevenvr.com/en/) points from a recording.
+
+You might use this script in a number ways, such as creating a video that removes all down-time from a recording. By default, it will find the points in the match and print their beginning and ending time points. This list can be saved to a csv file.
+
+Optionally, you can have it create video clips of points for you. It will produce individual clips of every point, as well as a single video file containing all clips.
+
+You might use this script to automatically filter out the non-action times from the entire recording. Or, you might use this script to find the top 10 clips from a match and automatically produce a video containing only those points.
+
+Overall, this should run pretty fast on modest hardware. For instance, I ran this on a 1.5 hour match with over 350 total points. Tt analyzed the video, produced an mp4 of every clip, and produced an mp4 of all the clips combined in less than 5 minutes total. It turned a 1.5 hour video into a 39 minute video of pure action.
+```
+Processing chunk 1 of 9, start:120.0s, end:689.0s ... completed in 20s
+Processing chunk 2 of 9, start:689.0s, end:1258.0s ... completed in 20s
+Processing chunk 3 of 9, start:1258.0s, end:1827.0s ... completed in 21s
+Processing chunk 4 of 9, start:1827.0s, end:2396.0s ... completed in 20s
+Processing chunk 5 of 9, start:2396.0s, end:2965.0s ... completed in 20s
+Processing chunk 6 of 9, start:2965.0s, end:3534.0s ... completed in 22s
+Processing chunk 7 of 9, start:3534.0s, end:4103.0s ... completed in 20s
+Processing chunk 8 of 9, start:4103.0s, end:4672.0s ... completed in 21s
+Processing chunk 9 of 9, start:4672.0s, end:5240.0s ... completed in 21s
+...
+Creating video clips ...
+Done! Created 379 video clips in 111s
+```
+
+There are a number of options you can use to affect the bevhaior discussed below.
+
+## How to use it
+
+To get help and see a description of the commands, run:
+```python
+python clipper.py -h
+```
+
+To get a csv file of all the points in the video and their start/end times, run:
+```python
+python clipper.py <video-file> --outcsv
+```
+
+To get a list of the top-10 longest points in the video and their start/end times, run:
+```python
+python clipper.py <video-file> --nclips 10 --orderby duration
+```
+
+To create a full video of all points without the dead time in-between points, run:
+```python
+python clipper.py <video-file> --outclips
+```
+
+To get a list of the top-10 points (by duration) and produce video clips of them, run:
+```python
+python clipper.py <video-file> --outclips --nclips 10 --orderby duration
+```
+
+Other options to consider:
+* Use `--starttime <x>` to specify that you only want to gather clips after x seconds. For instance, you probably don't want to include any warmup before a match.
+* If things are not working well, try tweaking `--delta` and `--min_centroid` options. These affect the sound detection aspect.
+
+
+## How it Works
+
+This script finds clips based on the sound of a ball hitting a paddle. It uses [onset detection](librosa.onset.onset_detect), which works well for these sounds. Initially I had a recording of several paddle hits and tried using cross correlation to find other matches in the video, but this had had false positives and false negatives, though it might have worked OK. Onset detection ended up being simply and performed better. Due to memory constraints, this process needed to be performed in chunks.
+
+Once the timestamps are collected for every paddle hit, groups are determined based on gaps between the timestamps; i.e., sounds close together are part of the same point.
+
+Then, ffmpeg is used to clip the timepoints from the main video file. This process is very fast assuming you do not re-encode the video. Unfortunately, if your timestamp falls on an I-frame (and not a keyframe) you must re-encode the video else the video will not display properly. To prevent, this, we find all the keyframes of the video and sync the starting timestamp of our cuts to the keyframe. **If the video has sparse keyframes, this method would be problematic.**
+
+## Other notes
+
+* This method is not perfect (even though it works reasonably well). If the point ends with the ball still bouncing on the table, this method thinks the point is still happening. Or, if someone is hitting a ball in between matches, we'd pick that up as a point.
+* I would have liked to have a more accurate count of the number of shots, but I could not figure out how to do this cleanly in the amount of time I spent on this project. Without a more complex model, it's tough to disambiguate the ball hitting the paddle vs. the ball hitting the table vs. the ball hitting the floor.
---
+++ clipper.py
@@ -0,0 +1,304 @@
+import os
+import glob
+import math
+import bisect
+import argparse
+import subprocess
+import librosa
+import pandas as pd
+import numpy as np
+import time
+
+TMP_AUDIO_FILE = "__tmp__.wav"
+TMP_CONCAT_FILE = "__tmp_concat__.txt"
+
+argparser = argparse.ArgumentParser(description="Extract clips of ETT points from video based on sounds.")
+argparser.add_argument("video_file", help="Path to the video file.")
+argparser.add_argument(
+    "--buffer",
+    type=float,
+    default=1,
+    help="buffer of time to add at the start and end of each clip",
+)
+argparser.add_argument(
+    "--outcsv",
+    action="store_true",
+    help="whether to output the point timestamps to a CSV file",
+)
+argparser.add_argument(
+    "--outclips",
+    action="store_true",
+    help="whether to create an output video for each clip and a combined video of all clips",
+)
+argparser.add_argument(
+    "--outname",
+    default="clips",
+    help="prefix to use for the output video files and data",
+)
+argparser.add_argument(
+    "--orderby",
+    default="chrono",
+    choices=["chrono", "duration"],
+    help="order for outputting the clips by the given metric",
+)
+argparser.add_argument(
+    "--nclips",
+    type=int,
+    help="number of clips to extract",
+)
+argparser.add_argument(
+    "--starttime",
+    type=int,
+    default=0,
+    help="seconds from the start of the video to start extracting clips",
+)
+argparser.add_argument(
+    "--delta",
+    type=float,
+    default=0.2,
+    help="threshold for onset detection",
+)
+argparser.add_argument(
+    "--min_centroid",
+    type=float,
+    default=1500,
+    help="minimum spectral centroid for onset detection",
+)
+
+
+def main():
+    args = argparser.parse_args()
+
+    try:
+        preclean(args.outname, args.outclips)  # ffmpeg hangs if output files already exist
+        audio_file = extract_audio_from_video(args.video_file)
+        timestamps = detect_onsets(
+            audio_file, start_time=args.starttime, delta=args.delta, min_centroid=args.min_centroid
+        )
+        points_df = get_points(timestamps)
+
+        # Do any sorting or limiting of the clips
+        if args.orderby == "duration":
+            points_df = points_df.sort_values("duration", ascending=False)
+
+        if args.nclips:
+            points_df = points_df.head(args.nclips)
+
+        points_df = points_df.round(2)
+
+        # Print and optionally write out the data
+        print()
+        print("Extracted points:")
+        print(points_df.to_markdown())
+        if args.outcsv:
+            points_df.to_csv(f"{args.outname}.csv", index=True, index_label="point")
+        print()
+
+        # Create clips and a combined video
+        if args.outclips:
+            print("Creating video clips ...")
+            st = time.time()
+            keyframes = get_keyframes(args.video_file)  # can only cut on a keyframe with re-encoding
+            segment_files = create_clips(
+                args.video_file,
+                points_df[["start", "end"]].values,
+                keyframes,
+                prefix=args.outname,
+                buffer=args.buffer,
+            )
+            concat_clips(segment_files, f"{args.outname}.mp4")
+            print(f"Done! Created {len(segment_files)} video clips in {round(time.time()-st)}s")
+
+    except:
+        raise
+    finally:
+        cleanup()
+
+
+def detect_onsets(audio_file, start_time=0, delta=0.2, min_centroid=1500, hop_length=32, chunk_size=600):
+    sr = librosa.get_samplerate(audio_file)
+    total_duration = librosa.get_duration(path=audio_file)
+
+    # Split up due to memory issues on large audio files
+    nchunks = math.ceil((total_duration - start_time) / chunk_size)
+    chunks = np.array_split(np.arange(start_time, total_duration), nchunks)
+    chunks = [(c[0], c[-1] + 1) for c in chunks]
+
+    filtered_onsets = []
+    for iter, (offset, end) in enumerate(chunks):
+        print(f"Processing chunk {iter+1} of {len(chunks)}, start:{offset}s, end:{end}s ... ", end="", flush=True)
+        st = time.time()
+        # Perform onset detection
+        audio, _ = librosa.load(audio_file, sr=sr, offset=offset, duration=(end - offset), mono=True)
+        onset_frames = librosa.onset.onset_detect(y=audio, backtrack=True, delta=delta, hop_length=hop_length)
+
+        # Convert frames to timestamps
+        onset_times = librosa.frames_to_time(onset_frames, sr=sr, hop_length=hop_length)
+
+        # Spectral Centroid (brightness measure)
+        # Filter out low-frequency (floor hit) onsets
+        centroids = librosa.feature.spectral_centroid(y=audio, sr=sr, hop_length=hop_length)[0]
+
+        for i, onset in enumerate(onset_frames):
+            if centroids[onset] > min_centroid:  # Only keep high-frequency (sharp) sounds
+                filtered_onsets.append(onset_times[i] + offset)
+
+        print(f"completed in {round(time.time()-st)}s", flush=True)
+
+    return filtered_onsets
+
+
+def preclean(outname, outclips):
+    for f in [TMP_AUDIO_FILE, TMP_CONCAT_FILE]:
+        if os.path.exists(f):
+            os.remove(f)
+    if outclips:
+        for f in glob.glob(f"{outname}_*.mp4") + [f"{outname}.mp4"]:
+            if os.path.exists(f):
+                os.remove(f)
+
+
+def cleanup():
+    for f in [TMP_AUDIO_FILE, TMP_CONCAT_FILE]:
+        if os.path.exists(f):
+            os.remove(f)
+
+
+def extract_audio_from_video(video_file):
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-loglevel",
+            "error",  # Suppress ffmpeg
+            "-i",
+            video_file,  # Input video file.
+            "-vn",  # Disable video recording.
+            "-acodec",
+            "pcm_s16le",  # Use PCM 16-bit little endian codec for WAV.
+            "-ar",
+            "22050",  # Set the audio sampling rate to 22050 Hz.
+            TMP_AUDIO_FILE,  # Output WAV file.
+        ]
+    )
+    return TMP_AUDIO_FILE
+
+
+def create_groups(timestamps, min_size=3, max_time_diff=1):
+    groups = []
+    if len(timestamps) == 0:
+        return groups
+    group = [timestamps[0]]
+    for i in range(1, len(timestamps)):
+        if timestamps[i] - group[-1] <= max_time_diff:
+            group.append(timestamps[i])
+        else:
+            if len(group) >= min_size:
+                groups.append(group)
+            group = [timestamps[i]]
+    groups.append(group)
+    return groups
+
+
+def get_points(timestamps):
+    groups = create_groups(timestamps)
+    df = pd.DataFrame(columns=["start", "end", "duration"])
+    for i, group in enumerate(groups):
+        start = group[0]
+        end = group[-1]
+        duration = end - start
+        df.loc[i] = [start, end, duration]
+
+    return df
+
+
+def concat_clips(files, output_file):
+    with open(TMP_CONCAT_FILE, "w") as f:
+        for file in files:
+            f.write(f"file '{file}'\n")
+
+    # Concatenate the segments
+    concat_cmd = [
+        "ffmpeg",
+        "-f",
+        "concat",
+        "-safe",
+        "0",
+        "-i",
+        TMP_CONCAT_FILE,
+        "-c",
+        "copy",
+        output_file,  # Uses copy mode to avoid re-encoding
+    ]
+    subprocess.run(concat_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def get_keyframes(video_file):
+    """Find the nearest keyframe before or equal to target_start using ffprobe."""
+    result = subprocess.run(
+        [
+            "ffprobe",
+            "-loglevel",
+            "error",
+            "-select_streams",
+            "v",
+            "-skip_frame",
+            "nokey",
+            "-show_frames",
+            "-show_entries",
+            "frame=pkt_pts_time",
+            "-of",
+            "csv",
+            video_file,
+        ],
+        capture_output=True,
+        text=True,
+    )
+
+    keyframe_times = []
+    for line in result.stdout.split("\n"):
+        if not "frame," in line:
+            continue
+        time = line.split(",")[1]
+        keyframe_times.append(float(time))
+
+    return keyframe_times
+
+
+def create_clips(video_file, points, keyframes, prefix="clip", buffer=1):
+    segment_files = []
+    file_type = video_file.split(".")[-1]
+    # Without doing some re-encoding, the start of clips get messed up due to not matching a keyframe
+    for i, (start, end) in enumerate(points):
+        start = max(0, start - buffer)
+        end += buffer
+        segment_file = f"{prefix}_{i}.{file_type}"
+
+        # Get closest keyframe before start
+        # Otherwise, would need to re-encode, which is very slow
+        start_keyframe = bisect.bisect_left(keyframes, start) - 1
+        if start_keyframe < 0:
+            start_keyframe = 0
+        start = keyframes[start_keyframe]
+
+        cmd = [
+            "ffmpeg",
+            "-loglevel",
+            "error",
+            "-ss",
+            str(start),  # Fast seek to nearest keyframe
+            "-i",
+            video_file,
+            "-t",
+            str(end - start),  # Duration of the clip
+            "-c",
+            "copy",  # No re-encoding
+            segment_file,
+        ]
+        subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        segment_files.append(segment_file)
+
+    return segment_files
+
+
+if __name__ == "__main__":
+    main()
