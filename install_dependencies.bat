e3eceb5e9f3bbcf68dca3a0bcdce7cad0b364445
Steve Harenberg <harenbergsd@gmail.com>
2025-10-14 00:42:37 -0400

    small update for dependency install files

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,3 +1,14 @@
-pip install uv
+@echo off
+cd /d "%~dp0"
+
+winget install uv
+
+uv venv --clear
+
+call .venv\Scripts\activate.bat
+
 uv pip install -r requirements.txt
-PAUSE
+
+echo.
+echo Done. Press any key to exit.
+pause >nul
